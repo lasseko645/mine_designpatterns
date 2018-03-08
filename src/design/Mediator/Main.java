c5193b34451706a8bbba3871864040b10d3030c8
lasseko645 <31951011+lasseko645@users.noreply.github.com>
2018-03-08 09:50:10 +0100

    har uploaded mediator klassen

---
+++ src/design/Mediator/Main.java
@@ -0,0 +1,4 @@
+package design.Mediator;
+
+public class Main {
+}
---
+++ src/design/Mediator/MainTest.java
@@ -0,0 +1,11 @@
+package design.Mediator;
+
+public class MainTest {
+    public static void main(String[] args) {
+        User lasse = new User("Lasse");
+        User johan = new User("Johan");
+
+        lasse.sendMessage("det er sku mig !?!?!?!");
+        johan.sendMessage("det er sku ikke mig !?!?!?!");
+    }
+}
